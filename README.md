0dd2e1206aee51e5077f5fdfb58723d186ab57a6
ysf0 <dr4f3d4efrdew@yahoo.com>
2022-04-03 15:56:34 +0300

    updates

---
+++ README.md
@@ -0,0 +1,9 @@
+# Firefox user-js
+"__generated_personal_user.js__" is the config file which can be use on Firefox.
+
+It based on arkenfox's user.js. The generator (shell script) filters some configs which breaks most of web pages which I visit them daily.
+
+You can inject the configs to your Android via "javascript_for_execute_js_on_android.js" file.
+
+# Test
+You can test your browser from: https://github.com/arkenfox/user.js/wiki/Appendix-A-Test-Sites
---
+++ additional_personal.js
@@ -0,0 +1,10 @@
+user_pref("dom.webnotifications.enabled", false); // I personaly don't need this feature.
+user_pref("extensions.update.interval", 609600); // Once in 7 days
+user_pref("clipboard.autocopy", false); // this feature is exist only on linux. if autocopy enabled, firefox automatically copies text on every selection. I personaly don't need this feature.
+user_pref("intl.locale.requested", "en-US"); // language of interface of firefox. This is hidden (not visible) on about:config page.
+user_pref("extensions.pocket.enabled", false);
+user_pref("browser.library.activity-stream.enabled", false); // disable activity/history on new tab
+user_pref("browser.safebrowsing.downloads.enabled", false);
+user_pref("browser.safebrowsing.malware.enabled", false);
+user_pref("browser.safebrowsing.phishing.enabled", false);
+user_pref("privacy.donottrackheader.enabled", true);
